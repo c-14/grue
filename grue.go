e04bc0a9e4ea1b31381ca5d2331a3d601459ec8f
c_14 <git@c-14.de>
2021-04-12 14:32:41 +0200

    Bump version to -next

--- grue.go
+++ grue.go
@@ -10,7 +10,7 @@ import (
 	"github.com/c-14/grue/config"
 )
 
-const version = "0.3.0"
+const version = "0.3.1-next"
 
 func usage() string {
 	return `usage: grue [--help] {add|delete|fetch|import|init_cfg|list|rename} ...
